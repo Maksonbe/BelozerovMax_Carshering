8fdae4b325833314f7ce161b7befff02dd43166e
Belozerov Max <maks120848@gmail.com>
2026-05-05 16:27:40 +0300

    sdelal kapchy

--- CarharingApp/Views/AuthPage.xaml.cs
+++ CarharingApp/Views/AuthPage.xaml.cs
@@ -1,16 +1,41 @@
-﻿using System.Windows;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
 
 namespace CarharingApp.Views
 {
     public partial class AuthPage : Window
     {
-
+        private Image firstButton;
+        private bool isPuzzleSolved = false;
         public AuthPage()
         {
             InitializeComponent();
+
+            LoadPuzzle();
+
         }
         private void LoginButton_Click(object sender, RoutedEventArgs e)
         {
+            if (isPuzzleSolved)
+            {
+                MessageBox.Show("Решите капчу!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
             string login = LoginTextBox.Text.Trim();
             string password = PasswordBox.Password;
 
@@ -57,5 +82,87 @@ namespace CarharingApp.Views
             this.Close();
 
         }
+
+
+
+        private void LoadPuzzle()
+        {
+            var rnd = new Random();
+            var pices = Enumerable.Range(1, 4).ToList();
+            pices = pices.OrderBy(x => rnd.Next()).ToList();
+            pices.ForEach(x =>
+            {
+                var img = new Image
+                {
+                    Source = new BitmapImage(new Uri($"Images/{x}.png", UriKind.Relative)),
+                    Tag = x,
+                    Stretch = Stretch.Fill
+                };
+                img.MouseLeftButtonUp += Pices_Click;
+
+                PuzzleGrid.Children.Add(img);
+            });
+        }
+
+        private void CheckPuzzle()
+        {
+            if (PuzzleGrid.Children.OfType<Image>()
+                    .Select((img, i) => i + 1 == (int)img.Tag)
+                    .All(x => x))
+            {
+                isPuzzleSolved = true;
+                MessageBox.Show("Решено!");
+            }
+
+        }
+
+
+        private void Pices_Click(object sender, RoutedEventArgs e)
+        {
+            if (!(sender is Image clicked)) return;
+
+            if (firstButton == null)
+            {
+                firstButton = clicked;
+                firstButton.Opacity = 0.5;
+                return;
+            }
+            if (clicked != firstButton)
+            {
+                (firstButton.Source, clicked.Source) = (clicked.Source, firstButton.Source);
+                (firstButton.Tag, clicked.Tag) = (clicked.Tag, firstButton.Tag);
+
+            }
+            firstButton.Opacity = 1;
+            firstButton = null;
+            CheckPuzzle();
+
+
+        }
+
+        private void Pices_Click(object sender, MouseButtonEventArgs e)
+        {
+            if (sender is Image clicked)
+            {
+                if (firstButton == null)
+                {
+                    firstButton = clicked;
+                    firstButton.Opacity = 0.5;
+                    return;
+                }
+                if (clicked != firstButton)
+                {
+                    (firstButton.Source, clicked.Source) = (clicked.Source, firstButton.Source);
+                    (firstButton.Tag, clicked.Tag) = (clicked.Tag, firstButton.Tag);
+
+                }
+                firstButton.Opacity = 1;
+                firstButton = null;
+                CheckPuzzle();
+            }
+
+        }
+
+
     }
 }
